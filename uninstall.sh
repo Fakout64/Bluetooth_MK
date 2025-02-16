a162a80d66e085b65df57ffdc920bbad7211b2c1
Fakout64 <101669083+Fakout64@users.noreply.github.com>
2025-02-16 15:28:16 -0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -5,3 +5,6 @@ if [ -f "bluetooth.service.bk" ] ; then
     sudo systemctl daemon-reload
     sudo /etc/init.d/bluetooth start
 fi
+sudo systemctl stop init_bt_mk
+sudo systemctl disable init_bt_mk
+sudo rm /lib/systemd/system/init_bt_mk.service
