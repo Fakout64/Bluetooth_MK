e376ae5142dab4160bc5adc7d7ecc7d7e5b51e3a
Fakout64 <101669083+Fakout64@users.noreply.github.com>
2025-02-16 15:08:58 -0800

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,3 +1,10 @@
+# Changes start
+#!/bin/bash
+sleep 10
+sudo cp dbus/org.thanhle.btkbservice.conf /etc/dbus-1/system.d
+sudo systemctl restart dbus.service
+# Changes break
+
 #Stop the background process
 sudo hciconfig hci0 down
 sudo systemctl daemon-reload
@@ -20,3 +27,7 @@ tmux kill-window -t thanhle:app >/dev/null 2>&1
 tmux send-keys -t thanhle:app.0 'cd $C_PATH/server && sudo ./btk_server.py' C-m
 tmux send-keys -t thanhle:app.1 'cd $C_PATH/mouse  && reset' C-m
 tmux send-keys -t thanhle:app.2 'cd $C_PATH/keyboard  && reset' C-m
+
+# Changes resume
+echo -e "agent on\nagent NoInputNoOutput\ndefault-agent"
+# Changes end
